fddc468ba85077e680cc1c7e3f5ff19878cdc218
anthony <anthony.doeraene@hotmail.com>
2022-04-19 08:52:58 +0200

    fixed executionSpeed

--- executionSpeed.sh
+++ executionSpeed.sh
@@ -1,10 +1,11 @@
 export CFLAGS='-gdwarf-2 -gstrict-dwarf'
 make
-N=200
-TIMESTART=$EPOCHREALTIME
+N=50
+TIMESTART=$(date +%s.%N)
 for ((i = 0 ; i <= $N ; i++));
     do ./fec ./binary_exemple -f here.txt
 done
-TIME=$(echo "($EPOCHREALTIME-$TIMESTART)/$N * 1000" | bc -l)
+TIMEEND=$(date +%s.%N)
+TIME=$(echo "($TIMEEND-$TIMESTART)/$N * 1000.0" | bc -l)
 echo Execution time :  $TIME ms
 make clean
